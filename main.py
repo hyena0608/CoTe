7d6fcff0e58733b0354267856243efe4b7e575d4
Sleep <82203978+hyena0608@users.noreply.github.com>
2021-08-23 05:53:03 +0000

    210823

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def find_parent(parent, x):
-    if parent[x] != x:
-      parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
 
 def union_parent(parent, a, b):
   a = find_parent(parent, a)
@@ -12,21 +12,25 @@ def union_parent(parent, a, b):
     parent[a] = b
 
 v, e = map(int, input().split())
+
 parent = [0] * (v + 1)
 
 for i in range(1, v + 1):
   parent[i] = i
 
-for i in range(e):
-  a, b = map(int, input().split())
-  union_parent(parent, a, b)
+edges = []
+result = 0
 
-print('각 원소가 속한 집합: ', end = '')
-for i in range(1, v + 1):
-  print(find_parent(parent, i), end = ' ')
+for _ in range(e):
+  a, b, cost = map(int, input().split())
+  edges.append((cost, a, b))
 
-print()
+edges.sort()
 
-print('부모가 속한 노드', end = '')
-for i in range(1, v + 1):
-  print(parent[i], end = ' ')
+for edge in edges:
+  cost, a, b = edge
+  if find_parent(parent, a) != find_parent(parent, b):
+    union_parent(parent, a, b)
+    result += cost
+
+print(result)
